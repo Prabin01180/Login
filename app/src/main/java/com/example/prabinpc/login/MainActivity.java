67dc57fe37990834b1c2d7539c5d2fff9a6ce4f1
Prabin01180 <prabinlamichhane70@gmail.com>
2017-11-12 16:33:24 +0545

    layout change

--- app/src/main/java/com/example/prabinpc/login/MainActivity.java
+++ app/src/main/java/com/example/prabinpc/login/MainActivity.java
@@ -1,32 +1,23 @@
 package com.example.prabinpc.login;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
-    private EditText username;
-    private EditText password;
-    private Button login;
-
-
+    private TextInputLayout main_til_username;
+    private TextInputLayout main_til_password;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        username=(EditText)findViewById(R.id.username);
-        login=(Button)findViewById(R.id.button);
-
     }
-    private void validate(String UserName, String Password)
-    {
-        if((UserName == "admin")&&(Password == "1234"))
-        {
-            System.out.println("You are login");
-        }else{
-            System.out.println("Invalid Username and Password");
-        }
+
+    public void login(View view) {
+
     }
 }
